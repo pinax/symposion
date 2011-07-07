bb01458864c27302eb8a73538534fff56eb0b194
Brian Rosner <brosner@gmail.com>
2011-07-07 13:30:44 -0600

    we all can't be perfect

--- symposion/proposals/views.py
+++ symposion/proposals/views.py
@@ -30,7 +30,7 @@ def proposal_submit(request):
             speaker_profile = request.user.speaker_profile
         except ObjectDoesNotExist:
             return redirect("speaker_dashboard")
-    if settings.ACCEPTING_PROPOSALS:
+    if not settings.ACCEPTING_PROPOSALS:
         return redirect("speaker_dashboard")
     if request.method == "POST":
         form = ProposalSubmitForm(request.POST)
