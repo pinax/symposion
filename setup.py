b200e79580cf28c14c0a1bedd94ba34608a882b7
Brian Rosner <brosner@gmail.com>
2011-07-11 21:00:36 -0600

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "symposion",
-    version = "1.0a1.dev11",
+    version = "1.0a1.dev12",
     description = "collection of Pinax apps for conferences",
     url = "https://github.com/pinax/symposion",
     author = "Brian Rosner",
