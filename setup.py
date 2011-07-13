61acdf4e64651e1faa8baad5630cf3bc61313854
Brian Rosner <brosner@gmail.com>
2011-07-13 13:03:26 -0600

    fixed typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "symposion",
-    version = "1.0a1.dev13,
+    version = "1.0a1.dev13",
     description = "collection of Pinax apps for conferences",
     url = "https://github.com/pinax/symposion",
     author = "Brian Rosner",
