636eb35d2520e2d41478d6e9f1826daa400a6ddc
Patrick Altman <paltman@gmail.com>
2011-07-18 18:59:06 -0500

    1.0a1.dev16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "symposion",
-    version = "1.0a1.dev15",
+    version = "1.0a1.dev16",
     description = "collection of Pinax apps for conferences",
     url = "https://github.com/pinax/symposion",
     author = "Brian Rosner",
