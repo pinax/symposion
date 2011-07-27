fbd32638dfb86b8d6e2fad2e8f650d314416c489
Brian Rosner <brosner@gmail.com>
2011-07-27 16:40:59 -0600

    fixed sponsor query to exclude sponsors with no weblogos correctly

--- symposion/sponsors_pro/templatetags/sponsors_pro_tags.py
+++ symposion/sponsors_pro/templatetags/sponsors_pro_tags.py
@@ -23,11 +23,34 @@ class SponsorsNode(template.Node):
         self.context_var = context_var
     
     def render(self, context):
-        queryset = Sponsor.objects.filter(
-            active = True,
-            sponsor_benefits__benefit__type="weblogo",
-            sponsor_benefits__upload__isnull=False
-            ).distinct().order_by("level", "added")
+        queryset = Sponsor.objects.raw("""
+        SELECT DISTINCT
+            "sponsors_pro_sponsor"."id",
+            "sponsors_pro_sponsor"."applicant_id",
+            "sponsors_pro_sponsor"."name",
+            "sponsors_pro_sponsor"."external_url",
+            "sponsors_pro_sponsor"."annotation",
+            "sponsors_pro_sponsor"."contact_name",
+            "sponsors_pro_sponsor"."contact_email",
+            "sponsors_pro_sponsor"."level_id",
+            "sponsors_pro_sponsor"."added",
+            "sponsors_pro_sponsor"."active",
+            "sponsors_pro_sponsorlevel"."order"
+        FROM
+            "sponsors_pro_sponsor"
+            INNER JOIN
+                "sponsors_pro_sponsorbenefit" ON ("sponsors_pro_sponsor"."id" = "sponsors_pro_sponsorbenefit"."sponsor_id")
+            INNER JOIN
+                "sponsors_pro_benefit" ON ("sponsors_pro_sponsorbenefit"."benefit_id" = "sponsors_pro_benefit"."id")
+            LEFT OUTER JOIN
+                "sponsors_pro_sponsorlevel" ON ("sponsors_pro_sponsor"."level_id" = "sponsors_pro_sponsorlevel"."id")
+        WHERE (
+            "sponsors_pro_sponsor"."active" = 't' AND
+            "sponsors_pro_benefit"."type" = 'weblogo' AND
+            "sponsors_pro_sponsorbenefit"."upload" != ''
+        )
+        ORDER BY "sponsors_pro_sponsorlevel"."order" ASC, "sponsors_pro_sponsor"."added" ASC
+        """)
         context[self.context_var] = queryset
         return u""
 
