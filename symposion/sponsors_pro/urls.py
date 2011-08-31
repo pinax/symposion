1aba00f1e58f3455b67b25c6639cafb5afc822b3
Luke Hatcher <lukeman@gmail.com>
2011-08-31 11:41:04 -0400

    add jobs list url pattern

--- symposion/sponsors_pro/urls.py
+++ symposion/sponsors_pro/urls.py
@@ -5,6 +5,7 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns("symposion.sponsors_pro.views",
     url(r"^$", direct_to_template, {"template": "sponsors/list.html"}, name="sponsor_list"),
     url(r"^info/$", "sponsor_info", name="sponsor_info"),
+    url(r"^jobs/$", direct_to_template, {"template": "sponsors/jobs.html"}, name="sponsor_jobs"),
     url(r"^apply/$", "sponsor_apply", name="sponsor_apply"),
     url(r"^terms/$", direct_to_template, {"template": "sponsors/terms.html"}, name="sponsor_terms"),
     url(r"^(?P<pk>\d+)/$", "sponsor_detail", name="sponsor_detail"),
