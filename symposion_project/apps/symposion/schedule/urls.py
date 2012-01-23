8b2ac9df90ee1efe7ec6865d8283403e6355740e
Luke Hatcher <lukeman@gmail.com>
2012-01-22 20:16:48 -0500

    remove old url entry

--- symposion_project/apps/symposion/schedule/urls.py
+++ symposion_project/apps/symposion/schedule/urls.py
@@ -20,7 +20,6 @@ urlpatterns = patterns("symposion.schedule.views",
     url(r"^track/(\d+)/$", "track_detail", name="schedule_track_detail"),
     url(r"^track/none/$", "track_detail_none", name="schedule_notrack_detail"),
     url(r"^session/(\d+)/$", "session_detail", name="schedule_session_detail"),
-    url(r"^tutorial/(\d+)/$", "tutorial_detail", name="schedule_tutorial_detail"),
     url(r"^bookmarks/(\d+)/(\w+)/$", "schedule_user_bookmarks", name="schedule_user_bookmarks"),
     url(r"^json/$", "schedule_json", name="schedule_json"),
 )
