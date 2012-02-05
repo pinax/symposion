0f6843b08dcf3010e037e99f70f87616be591868
James Tauber <jtauber@jtauber.com>
2012-02-05 13:53:49 -0500

    pinax account project

---
+++ symposion_project/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__about__ = """
+This project takes the zero_project and adds basic account management
+functionality such as sign up, log in, password change/reset and email
+confirmation. It is a foundation suitable for most sites that have user
+accounts.
+"""
