a3547d0ff1cc6dac77cf53330ff6043136e317c1
Horst Gutmann <zerok@zerokspot.com>
2012-03-23 23:33:00 -0400

    Leave view and improved redirecting

--- symposion_project/apps/proposals/admin.py
+++ symposion_project/apps/proposals/admin.py
@@ -4,5 +4,5 @@ from . import models
 
 
 admin.site.register(models.Proposal,
-    list_display=("title", "kind", "conference", "duration"),
+    list_display=("title", "kind", "conference", "duration", "speaker"),
     list_filter=("conference", "kind", "duration"))
--- symposion_project/apps/proposals/views.py
+++ symposion_project/apps/proposals/views.py
@@ -1,6 +1,9 @@
+import urlparse
+
 from django.views import generic as generic_views
 from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
 from django.contrib import messages
 
 from conference.models import current_conference
@@ -9,7 +12,21 @@ from . import forms
 from . import models
 
 
-class SubmitProposalView(generic_views.CreateView):
+class NextRedirectMixin(object):
+    """
+    A simple mixin for checking for a next parameter for redirects.
+    """
+    redirect_param = 'next'
+
+    def get_next_redirect(self):
+        next = self.request.GET.get(self.redirect_param)
+        netloc = urlparse.urlparse(next)[1]
+        if netloc is None or netloc == "" or netloc == self.request.get_host():
+            return next
+        return None
+
+
+class SubmitProposalView(generic_views.CreateView, NextRedirectMixin):
     """
     Once registered a user can submit a proposal for the conference for a
     specific kind. This is only possible while the selected SessionKind
@@ -17,6 +34,12 @@ class SubmitProposalView(generic_views.CreateView):
     """
     form_class = forms.ProposalSubmissionForm
     model = models.Proposal
+
+    def get_success_url(self):
+        next = self.get_next_redirect()
+        if next:
+            return next
+        return super(SubmitProposalView, self).get_success_url()
 
     def form_valid(self, form):
         obj = form.save(commit=False)
@@ -37,8 +60,32 @@ class SingleProposalView(generic_views.DetailView):
     def get_queryset(self):
         return self.model.objects.filter(conference=current_conference())
 
+    def get_context_data(self, **kwargs):
+        data = super(SingleProposalView, self).get_context_data(**kwargs)
+        data['can_leave'] = self.request.user in [s.user for s in self.object.additional_speakers.all()]
+        data['can_edit'] = self.request.user == self.object.speaker.user
+        data['can_delete'] = self.request.user.is_staff or self.request.user == self.object.speaker.user
+        return data
 
-class PermissionCheckedUpdateView(generic_views.UpdateView):
+
+class PermissionCheckedUpdateView(generic_views.UpdateView, NextRedirectMixin):
+    """
+    Base update class that extends the UpdateView with an additional call
+    of check_permissions.
+    """
+    def get_success_url(self):
+        next = self.get_next_redirect()
+        if next:
+            return next
+        return super(SubmitProposalView, self).get_success_url()
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(PermissionCheckedUpdateView, self).get_context_data(*args, **kwargs)
+        ctx.update({
+            'next': self.get_success_url()
+            })
+        return ctx
+
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
         resp = self.check_permissions()
@@ -77,7 +124,44 @@ class EditProposalView(PermissionCheckedUpdateView):
         return None
 
 
-class CancelProposalView(generic_views.DetailView):
+class AbstractProposalAction(generic_views.DetailView, NextRedirectMixin):
+    model = models.Proposal
+
+    def check_permissions(self):
+        pass
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        resp = self.check_permissions()
+        if resp is not None:
+            return resp
+        return super(AbstractProposalAction, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        resp = self.check_permissions()
+        if resp is not None:
+            return resp
+        resp = self.action()
+        if resp is not None:
+            return resp
+        return HttpResponseRedirect(self.get_post_action_url())
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(AbstractProposalAction, self).get_context_data(*args, **kwargs)
+        ctx.update({
+            'next': self.get_post_action_url()
+            })
+        return ctx
+
+    def get_post_action_url(self):
+        next = self.get_next_redirect()
+        if next:
+            return next
+        return reverse('my_proposals')
+
+
+class CancelProposalView(AbstractProposalAction):
     """
     During the submission and review period a proposal can be cancelled
     by the primary speaker. As soon as the review period is over
@@ -99,35 +183,48 @@ class CancelProposalView(generic_views.DetailView):
             return HttpResponseRedirect(self.object.get_absolute_url())
         return None
 
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        resp = self.check_permissions()
-        if resp is not None:
-            return resp
-        return super(CancelProposalView, self).get(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        resp = self.check_permissions()
-        if resp is not None:
-            return resp
+    def action(self):
         self.object.delete()
         messages.success(self.request, _("Proposal has been removed"))
-        return HttpResponseRedirect('/')
+        return None
 
 
-class LeaveProposalView(generic_views.View):
+class LeaveProposalView(CancelProposalView):
     """
     A secondary speaker can decide not to actually take part in a session
     and therefor leave a proposal. This is an option that is exclusive
     to secondary speakers and is not available to the primary speaker.
     """
-    pass
+    template_name_suffix = "_leave_confirm"
+
+    def check_permissions(self):
+        user = self.request.user
+        kind = self.object.kind
+        if not kind.accepts_proposals():
+            messages.error(self.request, _("You can no longer leave this proposal because the submission period has already ended."))
+            return HttpResponseRedirect(self.object.get_absolute_url())
+        if user not in [s.user for s in self.object.additional_speakers.all()]:
+            messages.error(self.request, _("Only secondary speakers can leave a proposal"))
+            return HttpResponseRedirect(self.object.get_absolute_url())
+        return None
+
+    def action(self):
+        self.object.additional_speakers.remove(self.request.user.speaker_profile)
+        messages.success(self.request, _("You were successfully removed as secondary speaker."))
 
 
-class ListProposalsView(generic_views.ListView):
+class ListUserProposalsView(generic_views.TemplateView):
     """
-    A speaker can see and manage a list of proposal submitted by her or that
-    includes her as secondary speaker.
+    A speaker can see and manage a list of proposals submitted by her or that
+    include her as a secondary speaker.
     """
-    pass
+    template_name = 'proposals/proposal_list_mine.html'
+
+    def get_context_data(self, **kwargs):
+        this_speaker = self.request.user.speaker_profile
+        ctx = super(ListUserProposalsView, self).get_context_data(**kwargs)
+        ctx.update({
+            'proposals': this_speaker.proposals.all(),
+            'proposal_participations': this_speaker.proposal_participations.all()
+            })
+        return ctx
