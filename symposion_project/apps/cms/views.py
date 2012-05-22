b65a953ba5f6d4fb9d29ca9cfa0bf73166a805a7
Luke Hatcher <lukeman@gmail.com>
2012-05-22 01:22:32 -0400

    get object or 404 on page objects

--- symposion_project/apps/cms/views.py
+++ symposion_project/apps/cms/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 from cms.models import Page
@@ -6,7 +6,7 @@ from cms.models import Page
 
 def page(request, slug):
     
-    page = Page.objects.get(path=slug)
+    page = get_object_or_404(Page, path=slug)
     siblings = page.get_siblings(include_self=True)
     
     return render_to_response("cms/page_detail.html", {
