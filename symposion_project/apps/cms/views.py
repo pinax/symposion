b20c348d57d2c345ef0bd164ed8c2a474d9c32ee
Luke Hatcher <lukeman@gmail.com>
2012-05-22 00:37:46 -0400

    add sibling pages to context

--- symposion_project/apps/cms/views.py
+++ symposion_project/apps/cms/views.py
@@ -7,7 +7,9 @@ from cms.models import Page
 def page(request, slug):
     
     page = Page.objects.get(path=slug)
+    siblings = page.get_siblings(include_self=True)
     
     return render_to_response("cms/page_detail.html", {
         "page": page,
+        "siblings": siblings,
     }, context_instance=RequestContext(request))
