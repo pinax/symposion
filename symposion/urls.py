02319e51c86580aecac797df75635de5e701a463
James Tauber <jtauber@jtauber.com>
2012-05-29 15:16:41 -0400

    updated urls

--- symposion/urls.py
+++ symposion/urls.py
@@ -1,29 +1,24 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
+from django.conf.urls.static import static
+
 from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.autodiscover()
 
-from pinax.apps.account.openid_consumer import PinaxConsumer
-
-
-handler500 = "pinax.views.server_error"
+# from pinax.apps.account.openid_consumer import PinaxConsumer
 
 
 urlpatterns = patterns("",
     url(r"^$", direct_to_template, {
         "template": "homepage.html",
     }, name="home"),
-    url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
     url(r"^admin/", include(admin.site.urls)),
-    url(r"^about/", include("about.urls")),
-    url(r"^account/", include("pinax.apps.account.urls")),
-    url(r"^openid/", include(PinaxConsumer().urls)),
+    url(r"^about/", include("symposion.about.urls")),
+    url(r"^account/", include("account.urls")),
+    # url(r"^openid/", include(PinaxConsumer().urls)),
 )
 
 
-if settings.SERVE_MEDIA:
-    urlpatterns += patterns("",
-        url(r"", include("staticfiles.urls")),
-    )
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
