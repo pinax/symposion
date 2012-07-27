10c2179056b94d85cea29594600abdcca7ec7263
Luke Hatcher <lukeman@gmail.com>
2012-07-27 01:57:43 -0400

    arrange urls

--- symposion_project/urls.py
+++ symposion_project/urls.py
@@ -18,18 +18,17 @@ urlpatterns = patterns("",
         "template": "homepage.html",
     }, name="home"),
     url(r"^admin/", include(admin.site.urls)),
-    
     url(r"^account/signup/$", symposion.views.SignupView.as_view(), name="account_signup"),
     url(r"^account/login/$", symposion.views.LoginView.as_view(), name="account_login"),
     url(r"^account/", include("account.urls")),
+    url(r"^markitup/", include("markitup.urls")),
     
+    # Symposion
     url(r"^dashboard/", symposion.views.dashboard, name="dashboard"),
     url(r"^speaker/", include("symposion.speakers.urls")),
     url(r"^proposals/", include("symposion.proposals.urls")),
     url(r"^sponsors/", include("symposion.sponsorship.urls")),
     url(r"^boxes/", include("symposion.boxes.urls")),
-    url(r"^markitup/", include("markitup.urls")),
-    
     url(r"^", include("symposion.cms.urls")),
 )
 
