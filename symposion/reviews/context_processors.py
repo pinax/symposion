dde5b282eab84097f3e67e675d0abecde630006e
Luke Hatcher <lukeman@gmail.com>
2012-08-01 03:32:56 -0400

    add kinds to context

--- symposion/reviews/context_processors.py
+++ symposion/reviews/context_processors.py
@@ -1,6 +1,14 @@
-def permissions(request):
+from django.contrib.contenttypes.models import ContentType
+
+from symposion.proposals.models import ProposalSection
+
+
+def reviews(request):
+    kinds = []
+    for proposal_section in ProposalSection.available():
+        for kind in proposal_section.section.proposal_kinds.all():
+            kinds.append(kind)
+    # @@@ need to check perms here and include in context
     return {
-        "is_reviewer": request.user.groups.filter(name="reviewers").exists(),
-        "is_reviewer_tutorials": request.user.groups.filter(name="reviewers-tutorials").exists(),
-        "is_reviewer_admin": request.user.groups.filter(name="reviewers-admins").exists(),
+        "review_kinds": kinds,
     }
