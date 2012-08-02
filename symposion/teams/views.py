4f888ffd576e0e714ba68f5042f1619a3b0f9972
James Tauber <jtauber@jtauber.com>
2012-08-01 22:28:57 -0400

    include state in team detail context vars

--- symposion/teams/views.py
+++ symposion/teams/views.py
@@ -9,9 +9,11 @@ from symposion.teams.models import Team
 @login_required
 def team_detail(request, slug):
     team = get_object_or_404(Team, slug=slug)
-    if team.access == "invitation" and team.get_state_for_user(request.user) is None:
+    state = team.get_state_for_user(request.user)
+    if team.access == "invitation" and state is None:
         raise Http404()
     
     return render(request, "teams/team_detail.html", {
         "team": team,
+        "state": state,
     })
