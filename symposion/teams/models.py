0dbcbea1008aa923fa46c5bffd7bb2d3d0a1361e
James Tauber <jtauber@jtauber.com>
2012-08-07 23:28:15 -0400

    added manager permissions

--- symposion/teams/models.py
+++ symposion/teams/models.py
@@ -20,7 +20,13 @@ class Team(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     access = models.CharField(max_length=20, choices=TEAM_ACCESS_CHOICES)
-    permissions = models.ManyToManyField(Permission, blank=True)
+
+    # member permissions
+    permissions = models.ManyToManyField(Permission, blank=True, related_name="member_teams")
+    
+    # manager permissions
+    manager_permissions = models.ManyToManyField(Permission, blank=True, related_name="manager_teams")
+    
     created = models.DateTimeField(default=datetime.datetime.now, editable=False)
     
     def __unicode__(self):
