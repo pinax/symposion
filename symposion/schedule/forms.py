224b9c8d57bf3bb1853aa28334c4c9daa834ecd2
Luke Hatcher <lukeman@gmail.com>
2012-08-31 01:00:56 -0400

    remove slot pk from form

--- symposion/schedule/forms.py
+++ symposion/schedule/forms.py
@@ -9,16 +9,3 @@ class SlotEditForm(forms.Form):
         queryset=Presentation.objects.filter(slot__isnull=True),
         required=True,
     )
-    slot_pk = forms.CharField(
-        max_length=10,
-        widget=forms.HiddenInput,
-        required=False,
-    )
-    
-    def clean_slot_pk(self):
-        value = self.cleaned_data["slot_pk"]
-        try:
-            Slot.objects.get(pk=value)
-        except Slot.DoesNotExist:
-            raise forms.ValidationError("Invalid slot.")
-        return value
