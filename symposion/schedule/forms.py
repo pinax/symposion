1871be41e96171a32854bb9cfac9baffb82ff60d
Luke Hatcher <lukeman@gmail.com>
2012-08-31 02:30:22 -0400

    empty label

--- symposion/schedule/forms.py
+++ symposion/schedule/forms.py
@@ -8,4 +8,5 @@ class SlotEditForm(forms.Form):
     presentation = forms.ModelChoiceField(
         queryset=Presentation.objects.filter(slot__isnull=True),
         required=True,
+        empty_label=None,
     )
