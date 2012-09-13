4fc3eb4a10fbbeb26466a1859f40f4b44eb77aa2
Brian Rosner <brosner@gmail.com>
2012-09-13 14:51:21 -0600

    removed empty_label=None on SlotEditForm.presentation

--- symposion/schedule/forms.py
+++ symposion/schedule/forms.py
@@ -8,5 +8,4 @@ class SlotEditForm(forms.Form):
     presentation = forms.ModelChoiceField(
         queryset=Presentation.objects.filter(slot__isnull=True),
         required=True,
-        empty_label=None,
     )
