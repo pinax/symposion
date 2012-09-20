f1072eb4d9a16f250d069390c6ee7266901c3717
Brian Rosner <brosner@gmail.com>
2012-09-19 20:05:05 -0600

    fixed schedule URLs

--- symposion/schedule/urls.py
+++ symposion/schedule/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls.defaults import url, patterns
 urlpatterns = patterns("symposion.schedule.views",
     url(r"^$", "schedule_detail", name="schedule_detail_slugless"),
     url(r"^edit/$", "schedule_edit", name="schedule_edit_slugless"),
-    url(r"^(\w+)/edit/$", "schedule_detail", name="schedule_detail"),
+    url(r"^list/$", "schedule_list", name="schedule_list_slugless"),
+    url(r"^(\w+)/$", "schedule_detail", name="schedule_detail"),
     url(r"^(\w+)/edit/$", "schedule_edit", name="schedule_edit"),
     url(r"^(\w+)/edit/slot/(\d+)/", "schedule_slot_edit", name="schedule_slot_edit"),
-    url(r"^list/$", "schedule_list", name="schedule_list_slugless"),
     url(r"^(\w+)/list/$", "schedule_list", name="schedule_list"),
 )
