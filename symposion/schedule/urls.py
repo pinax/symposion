17869c7c7cbf985e67c9c6c712c84e0cb2ebfbaa
Brian Rosner <brosner@gmail.com>
2012-09-19 20:06:18 -0600

    improved ordering of URLs

--- symposion/schedule/urls.py
+++ symposion/schedule/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns("symposion.schedule.views",
     url(r"^list/$", "schedule_list", name="schedule_list_slugless"),
     url(r"^(\w+)/$", "schedule_detail", name="schedule_detail"),
     url(r"^(\w+)/edit/$", "schedule_edit", name="schedule_edit"),
-    url(r"^(\w+)/edit/slot/(\d+)/", "schedule_slot_edit", name="schedule_slot_edit"),
     url(r"^(\w+)/list/$", "schedule_list", name="schedule_list"),
+    url(r"^(\w+)/edit/slot/(\d+)/", "schedule_slot_edit", name="schedule_slot_edit"),
 )
