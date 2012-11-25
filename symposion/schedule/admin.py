479579f35072302b4147c2811854965f4aafb5b5
Luke Hatcher <lukeman@gmail.com>
2012-11-25 15:10:30 -0500

    add some columns to schedule admin lists

--- symposion/schedule/admin.py
+++ symposion/schedule/admin.py
@@ -7,6 +7,12 @@ admin.site.register(Schedule)
 admin.site.register(Day)
 admin.site.register(Room)
 admin.site.register(SlotKind)
-admin.site.register(Slot)
-admin.site.register(SlotRoom)
+admin.site.register(
+    Slot,
+    list_display=("day", "start", "end", "kind")
+)
+admin.site.register(
+    SlotRoom,
+    list_display=("slot", "room")
+)
 admin.site.register(Presentation)
