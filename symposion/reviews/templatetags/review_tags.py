db7f8fac5d43fc501d4df4cb88899991ddf509b9
florapdx <floraworleypdx@gmail.com>
2013-04-25 15:37:38 -0700

    added user_not_reviewed to templatetags

--- symposion/reviews/templatetags/review_tags.py
+++ symposion/reviews/templatetags/review_tags.py
@@ -1,6 +1,7 @@
 from django import template
 
 from symposion.reviews.models import Review, ReviewAssignment
+from symposion.proposals.models import ProposalBase
 
 
 register = template.Library()
@@ -12,6 +13,12 @@ def user_reviews(context):
     reviews = Review.objects.filter(user=request.user)
     return reviews
 
+@register.assignment_tag(takes_context=True)
+def user_not_reviewed(context):
+	request = context["request"]
+	already_reviewed = [r.proposal_id for r in Review.objects.filter(user=request.user)]
+	yet_to_review = ProposalBase.objects.exclude(id__in=[p.id for p in ProposalBase.objects.filter(id__in=already_reviewed)]).exclude(speaker=request.user)
+	return yet_to_review
 
 @register.assignment_tag(takes_context=True)
 def review_assignments(context):
