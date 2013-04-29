6c5c70ab101573cec71f7d78752347ca4dfb4c6e
florapdx <floraworleypdx@gmail.com>
2013-04-29 14:13:10 -0700

    removed user_reviews template tag; now being handled by review_section view

--- symposion/reviews/templatetags/review_tags.py
+++ symposion/reviews/templatetags/review_tags.py
@@ -6,20 +6,6 @@ from symposion.proposals.models import ProposalBase
 
 register = template.Library()
 
-
-@register.assignment_tag(takes_context=True)
-def user_reviews(context):
-    request = context["request"]
-    reviews = Review.objects.filter(user=request.user)
-    return reviews
-
-@register.assignment_tag(takes_context=True)
-def user_not_reviewed(context):
-	request = context["request"]
-	already_reviewed = [r.proposal_id for r in Review.objects.filter(user=request.user)]
-	yet_to_review = ProposalBase.objects.exclude(id__in=[p.id for p in ProposalBase.objects.filter(id__in=already_reviewed)]).exclude(speaker=request.user)
-	return yet_to_review
-
 @register.assignment_tag(takes_context=True)
 def review_assignments(context):
     request = context["request"]
