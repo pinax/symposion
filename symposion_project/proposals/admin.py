30c919e7d191cda536d3adfc23581a5f6eb1e755
Luke Hatcher <lukeman@gmail.com>
2013-06-04 12:19:22 -0400

    remove starter project bits from the module repo
    see https://github.com/pinax/pinax-project-symposion

--- symposion_project/proposals/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from symposion_project.proposals.models import TalkProposal, TutorialProposal, PosterProposal
-
-
-admin.site.register(TalkProposal)
-admin.site.register(TutorialProposal)
-admin.site.register(PosterProposal)
--- symposion_project/wsgi.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "symposion_project.settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
