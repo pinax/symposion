cca9ad0793fa7ba5c6bacb364f7cc22efbb02525
David Ray <davidray@gmail.com>
2014-01-14 11:06:41 -0500

    add dependencies for proper migration order

--- symposion/proposals/migrations/0001_initial.py
+++ symposion/proposals/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("speakers", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding model 'ProposalSection'
         db.create_table('proposals_proposalsection', (
