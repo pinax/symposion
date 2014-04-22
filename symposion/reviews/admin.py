621124b1b2464a8c3b407fdc6e686b3c610f6504
codersquid <github@codersquid.com>
2014-04-22 10:02:33 -0500

    add Review to admin

--- symposion/reviews/admin.py
+++ symposion/reviews/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from symposion.reviews.models import NotificationTemplate
+from symposion.reviews.models import NotificationTemplate, Review
+
+admin.site.register(Review,
+        list_display = ['proposal', 'section', 'vote', 'user'],
+        list_filter = ['vote', 'section'],
+        date_heirarchy='submitted_at',
+        )
+
 
 
 admin.site.register(NotificationTemplate)
