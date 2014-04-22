b7d44c7eb93dc0bd88b867143adbee2477a36f7a
codersquid <github@codersquid.com>
2014-04-22 10:33:45 -0500

    cannot use section property in admin

--- symposion/reviews/admin.py
+++ symposion/reviews/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from symposion.reviews.models import NotificationTemplate, Review
 
 admin.site.register(Review,
-        list_display = ['proposal', 'section', 'vote', 'user'],
-        list_filter = ['vote', 'section'],
+        list_display = ['proposal', 'vote', 'user'],
+        list_filter = ['vote',],
         date_heirarchy='submitted_at',
         )
 
