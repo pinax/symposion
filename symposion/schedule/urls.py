51709c6eaf398c50cbeaf5032758e1c1c0c39438
Sheila Miguez <github@codersquid.com>
2014-09-20 20:20:04 -0500

    adds a schedule json endpoint. based on @taavi's PR #45 with some changes from the @pyohio/pyohio repo

--- symposion/schedule/urls.py
+++ symposion/schedule/urls.py
@@ -1,9 +1,5 @@
 # flake8: noqa
 from django.conf.urls.defaults import url, patterns
-from django.views.decorators.cache import cache_page
-
-from symposion.schedule.views import schedule_json
-
 
 urlpatterns = patterns("symposion.schedule.views",
     url(r"^$", "schedule_conference", name="schedule_conference"),
@@ -16,5 +12,5 @@ urlpatterns = patterns("symposion.schedule.views",
     url(r"^([\w\-]+)/list/$", "schedule_list", name="schedule_list"),
     url(r"^([\w\-]+)/presentations.csv$", "schedule_list_csv", name="schedule_list_csv"),
     url(r"^([\w\-]+)/edit/slot/(\d+)/", "schedule_slot_edit", name="schedule_slot_edit"),
-    url(r"^conference.json", cache_page(300)(schedule_json), name="schedule_json"),
+    url(r"^conference.json", "schedule_json", name="schedule_json"),
 )
--- symposion/schedule/views.py
+++ symposion/schedule/views.py
@@ -1,8 +1,13 @@
+from datetime import datetime
+import json
+
+from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader, Context
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.models import Site
 
 from symposion.schedule.forms import SlotEditForm
 from symposion.schedule.models import Schedule, Day, Slot, Presentation
@@ -159,46 +164,39 @@ def schedule_presentation_detail(request, pk):
 
 
 def schedule_json(request):
-    everything = bool(request.GET.get('everything'))
-    slots = Slot.objects.all().order_by("start")
+    slots = Slot.objects.filter(day__schedule__published=True, day__schedule__hidden=False).order_by("start")
+
+    protocol = request.META.get('HTTP_X_FORWARDED_PROTO', 'http')
     data = []
     for slot in slots:
-        if slot.content:
-            slot_data = {
+        slot_data = {
+            "room": ", ".join(room["name"] for room in slot.rooms.values()),
+            "rooms": [room["name"] for room in slot.rooms.values()],
+            "start": datetime.combine(slot.day.date, slot.start).isoformat(),
+            "end": datetime.combine(slot.day.date, slot.end).isoformat(),
+            "duration": slot.length_in_minutes,
+            "kind": slot.kind.label,
+            "section": slot.day.schedule.section.slug,
+        }
+        if hasattr(slot.content, "proposal"):
+            slot_data.update({
                 "name": slot.content.title,
-                "room": ", ".join(room["name"] for room in slot.rooms.values()),
-                "start": slot.start_datetime.isoformat(),
-                "end": slot.end_datetime.isoformat(),
-                "duration": slot.length_in_minutes,
                 "authors": [s.name for s in slot.content.speakers()],
-                "released": slot.content.proposal.recording_release,
-                # You may wish to change this...
-                "license": "All Rights Reserved",
-                "contact":
-                [s.email for s in slot.content.speakers()]
-                if request.user.is_staff
-                else ["redacted"],
+                "contact": [
+                    s.email for s in slot.content.speakers()
+                ] if request.user.is_staff else ["redacted"],
                 "abstract": slot.content.abstract.raw,
                 "description": slot.content.description.raw,
-                "conf_key": slot.content.pk,
-                "conf_url": "https://%s%s" % (
+                "content_href": "%s://%s%s" % (
+                    protocol,
                     Site.objects.get_current().domain,
                     reverse("schedule_presentation_detail", args=[slot.content.pk])
                 ),
-                "kind": slot.kind.label,
-                "tags": "",
-            }
-        elif everything:
-            slot_data = {
-                "room": ", ".join(room["name"] for room in slot.rooms.values()),
-                "start": slot.start_datetime.isoformat(),
-                "end": slot.end_datetime.isoformat(),
-                "duration": slot.length_in_minutes,
-                "kind": slot.kind.label,
-                "title": slot.content_override.raw,
-            }
+            })
         else:
-            continue
+            slot_data.update({
+                "name": slot.content_override.raw if slot.content_override else "Slot",
+            })
         data.append(slot_data)
 
     return HttpResponse(
