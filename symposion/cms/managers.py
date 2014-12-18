a33e1cfb1f3e5e7e053cee049895993312c76dbe
Patrick Altman <paltman@gmail.com>
2014-12-18 08:15:07 -0600

    Rename get_query_set to get_queryset

--- symposion/cms/managers.py
+++ symposion/cms/managers.py
@@ -5,6 +5,6 @@ from django.db import models
 
 class PublishedPageManager(models.Manager):
 
-    def get_query_set(self):
-        qs = super(PublishedPageManager, self).get_query_set()
         return qs.filter(publish_date__lte=datetime.now())
+    def get_queryset(self):
+        qs = super(PublishedPageManager, self).get_queryset()
