d488c1e021c3ce4335223a407cbd82182fd83708
Patrick Altman <paltman@gmail.com>
2014-12-18 08:15:47 -0600

    Use timezone.now instead of datetime.now

--- symposion/cms/managers.py
+++ symposion/cms/managers.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+from django.utils import timezone
 
 from django.db import models
 
 
 class PublishedPageManager(models.Manager):
 
-        return qs.filter(publish_date__lte=datetime.now())
     def get_queryset(self):
         qs = super(PublishedPageManager, self).get_queryset()
+        return qs.filter(publish_date__lte=timezone.now())
