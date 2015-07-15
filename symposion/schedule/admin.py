7cff2d8b1dcfb0a8b5dc06bbf1c5298f9f8c7153
Sheila Miguez <github@codersquid.com>
2015-07-15 16:43:44 -0500

    add to schedule admin

--- symposion/schedule/admin.py
+++ symposion/schedule/admin.py
@@ -5,11 +5,18 @@ from symposion.schedule.models import Schedule, Day, Room, SlotKind, Slot, SlotR
 
 admin.site.register(Schedule)
 admin.site.register(Day)
-admin.site.register(Room)
-admin.site.register(SlotKind)
+admin.site.register(
+    Room,
+    list_display=("name", "order", "schedule"),
+    list_filter=("schedule",)
+)
+admin.site.register(
+    SlotKind,
+    list_display=("label", "schedule"),
+)
 admin.site.register(
     Slot,
-    list_display=("day", "start", "end", "kind")
+    list_display=("day", "start", "end", "kind", "content")
 )
 admin.site.register(
     SlotRoom,
@@ -17,4 +24,7 @@ admin.site.register(
 )
 admin.site.register(Session)
 admin.site.register(SessionRole)
-admin.site.register(Presentation)
+admin.site.register(
+    Presentation,
+    list_filter=("section", "cancelled", "slot")
+)
