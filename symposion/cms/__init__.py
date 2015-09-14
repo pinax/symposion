a3c89ece12fa72602b0ae84243c4fd68bb360c33
Patrick Altman <paltman@gmail.com>
2015-09-13 21:43:02 -0500

    Use pinax-pages instead

--- symposion/cms/__init__.py
+++
@@ -1,2 +0,0 @@
-# @@@ Consider replacing with pinax-wiki [1]
-default_app_config = "symposion.cms.apps.CMSConfig"
--- symposion/cms/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CMSConfig(AppConfig):
-    name = "symposion.cms"
-    label = "symposion_cms"
-    verbose_name = "Symposion CMS"
